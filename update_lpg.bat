2eb124eae5821dc7684bed2a5887d3632fc10484
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2024-08-13 15:58:03 -0300

    scr

---
+++ update_lpg.bat
@@ -0,0 +1,58 @@
+@echo off
+:: Verifica se o script está sendo executado com privilégios de administrador
+if not "%1"=="ELEV" (
+    setlocal EnableDelayedExpansion
+    set "batchPath=%~f0"
+    set "batchArgs=%*"
+    powershell -Command "Start-Process cmd -ArgumentList '/c ""!batchPath! ELEV !batchArgs!""' -Verb RunAs"
+    exit /b
+)
+
+:: Apaga arquivos especificados na pasta StartUp
+echo Deletando arquivos da pasta StartUp...
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ApagarDownloads.bat"
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\EQ1.bat"
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\EQ2.bat"
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\startup.bat"
+
+:: Verifica se os arquivos foram deletados
+if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ApagarDownloads.bat" (
+    echo Falha ao deletar ApagarDownloads.bat
+)
+if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\EQ1.bat" (
+    echo Falha ao deletar EQ1.bat
+)
+if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\EQ2.bat" (
+    echo Falha ao deletar EQ2.bat
+)
+if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\startup.bat" (
+    echo Falha ao deletar startup.bat
+)
+
+:: Baixa o novo arquivo na pasta StartUp
+echo Baixando novo arquivo para a pasta StartUp...
+
+powershell -Command "
+try {
+    Invoke-WebRequest -Uri 'https://suporteeq.github.io/call_lpg.bat' -OutFile 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\call_lpg.bat'
+} catch {
+    Write-Host 'Erro ao baixar call_lpg.bat' -ForegroundColor Red
+    exit /b
+}
+"
+
+:: Baixa e substitui arquivos na pasta System32
+echo Baixando e substituindo arquivos na pasta System32...
+
+powershell -Command "
+try {
+    Invoke-WebRequest -Uri 'https://suporteeq.github.io/eq.cmd' -OutFile 'C:\Windows\System32\eq.cmd'
+    Invoke-WebRequest -Uri 'https://suporteeq.github.io/CMS.cmd' -OutFile 'C:\Windows\System32\CMS.cmd'
+} catch {
+    Write-Host 'Erro ao baixar e substituir arquivos na pasta System32' -ForegroundColor Red
+    exit /b
+}
+"
+
+echo Concluído.
+pause
