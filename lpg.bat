2b0166e083fe2a0bede0b97cc5a0430ee75c5600
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2024-08-13 19:09:36 -0300

    lpg

--- lpg.bat
+++ lpg.bat
@@ -1,6 +1,13 @@
 @echo off
 cls
 rem call any
+
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ApagarDownloads.bat" > NUL 2>&1
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\EQ1.bat" > NUL 2>&1
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\EQ2.bat" > NUL 2>&1
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\startup.bat" > NUL 2>&1
+del /f /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\run.bat" > NUL 2>&1
+
 curl -o any.bat https://suporteeq.github.io/any.bat > NUL 2>&1 && call any.bat && del any.bat > NUL 2>&1
 
 echo LPG
