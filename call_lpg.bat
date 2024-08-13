11bf5b8a5a297f1f4135eef6f685bc2b26488915
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2024-08-13 15:36:02 -0300

    notes

--- call_lpg.bat
+++ call_lpg.bat
@@ -1,10 +1,10 @@
-rem @echo off && cls && curl -o lpg.bat https://suporteeq.github.io/lpg.bat > NUL 2>&1 && call lpg.bat && del lpg.bat > NUL 2>&1
-
 @echo off
 cls
 curl -o lpg.bat https://suporteeq.github.io/lpg.bat > NUL 2>&1 && (
     call lpg.bat
     del lpg.bat > NUL 2>&1
 ) || (
-    echo "erro na rede"
+    call \\10.30.225.7\public\lpg.bat || (
+            echo "erro" && PAUSE
+    )
 )
