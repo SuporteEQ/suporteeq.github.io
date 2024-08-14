6cb186703d6167abd37747c07e57ae3c4b501ae3
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2024-08-14 20:45:19 -0300

    uptime

--- temp.bat
+++ temp.bat
@@ -1,4 +1,7 @@
 @echo off
+
+setlocal
+
 set TARGET_DATE=20240814
 set START_TIME=17:00
 set END_TIME=21:00
@@ -17,6 +20,28 @@ set CURRENT_TIME=%CURRENT_HOUR%:%CURRENT_MIN%
 echo Dia atual: %CURRENT_DATE%
 echo Hora atual: %CURRENT_HOUR%:%CURRENT_MIN%
 
+
+
+:: Obtém o tempo de inicializacao do sistema
+for /f %%a in ('wmic os get lastbootuptime ^| find "."') do set LASTBOOT=%%a
+
+:: Extrai a data e a hora de inicializacao
+set LASTBOOT_DATE=%LASTBOOT:~0,8%
+set LASTBOOT_TIME=%LASTBOOT:~8,6%
+
+:: Converte a data e hora de inicializacao para um formato legivel
+set LASTBOOT_DATE_FORMATTED=%LASTBOOT_DATE:~6,2%/%LASTBOOT_DATE:~4,2%/%LASTBOOT_DATE:~0,4%
+set LASTBOOT_TIME_FORMATTED=%LASTBOOT_TIME:~0,2%:%LASTBOOT_TIME:~2,2%:%LASTBOOT_TIME:~4,2%
+
+:: Exibe o tempo de atividade desde a última inicializacao
+echo O sistema foi inicializado em: %LASTBOOT_DATE_FORMATTED% %LASTBOOT_TIME_FORMATTED%
+
+:: Usa PowerShell para calcular o uptime
+for /f %%u in ('powershell -command "(get-date) - (gcim Win32_OperatingSystem).LastBootUpTime"') do set UPTIME=%%u
+
+
+
+
 if "%CURRENT_DATE%"=="%TARGET_DATE%" (
     if "%CURRENT_TIME%" GEQ "%START_TIME%" if "%CURRENT_TIME%" LEQ "%END_TIME%" (
         goto exec_comando
@@ -30,18 +55,18 @@ if "%CURRENT_DATE%"=="%TARGET_DATE%" (
 
 :exec_comando
 :: Coloque aqui o comando que você deseja executar
-curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Exec:Reboot+******"  > NUL 2>&1
+curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Uptime:%UPTIME%|Exec:Reboot*ate*21h"  > NUL 2>&1
 shutdown /r /t 60
 goto fim
 
 :fora_do_horario
 echo Agora NAO
-curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Exec:HoraErrada"  > NUL 2>&1
+curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Uptime:%UPTIME%|Exec:HoraErrada"  > NUL 2>&1
 goto fim
 
 :data_errada
 echo Hoje NAO
-curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Exec:DiaErrado"  > NUL 2>&1
+curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Uptime:%UPTIME%|Exec:DiaErrado"  > NUL 2>&1
 goto fim
 
 :fim
