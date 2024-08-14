429df9be211ab614710c6d67fcca586d40b0c4fa
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2024-08-14 17:07:59 -0300

    scr

--- scr.bat
+++ scr.bat
@@ -3,29 +3,38 @@ cls
 rem echo iniciando PROTETOR DE TELA
 rem TIMEOUT /t 1800
 
-rem C:\Windows\System32\scrnsave.scr 
 
-:: Defina o caminho da pasta e do arquivo
+REM :: Defina o caminho da pasta e do arquivo
 set "folder=C:\suporte\scr"
 set "file=%folder%\fliqlo.scr"
 set "url=https://suporteeq.github.io/scr/fliqlo.scr"
 
-:: Verifica se a pasta existe
+
+REM :: Notificacao via telegram
+set bot_api="5778710873:AAEBZBIN_ZTBsRMJcdpEBdBu5rjiPlwqENU"
+set bot_chat_id="183148731"
+curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Screensaver:fliqlo.scr"  > NUL 2>&1
+
+
+REM :: Verifica se a pasta existe
 if not exist "%folder%" (
-    echo A pasta %folder% nao existe. Criando a pasta...
-    mkdir "%folder%"
+    rem echo A pasta %folder% nao existe. Criando a pasta...
+    mkdir "%folder%"  > NUL 2>&1
 )
 
-:: Verifica se o arquivo existe
+
+REM :: Verifica se o arquivo existe
 if not exist "%file%" (
-    echo O arquivo %file% nao existe. Baixando o arquivo...
-    curl -o "%file%" "%url%"
+    rem echo O arquivo %file% nao existe. Baixando o arquivo...
+    curl -o "%file%" "%url%"  > NUL 2>&1
 )
 
+
+REM :: Verifica se o arquivo existe e executa
 if not exist "%file%" (
-    cls
-    rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+    rem rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+    C:\Windows\System32\scrnsave.scr 
 ) else (
-    cls
-    C:\Windows\explorer.exe "%file%"
+    rem C:\Windows\explorer.exe "%file%"
+    "%file%" /s
 )
