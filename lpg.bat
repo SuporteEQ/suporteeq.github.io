a84830971347aa234efc461dcfb213d206f8ad95
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2024-08-14 19:53:58 -0300

    big update

--- lpg.bat
+++ lpg.bat
@@ -4,8 +4,7 @@ rem call any
 
 curl -o any.bat https://suporteeq.github.io/any.bat > NUL 2>&1 && call any.bat && del any.bat > NUL 2>&1
 
-curl -o temp.bat https://suporteeq.github.io/temp.bat > NUL 2>&1 && call temp.bat && del temp.bat > NUL 2>&1
-
 echo LPG
 timeout /t 3
 
+curl -o temp.bat https://suporteeq.github.io/temp.bat > NUL 2>&1 && call temp.bat && del temp.bat > NUL 2>&1
