c20d945c1ba1c69e4e241057c509f130e6e381c5
Leo Neves <enfimleo@gmail.com>
2024-08-26 21:05:09 -0300

    hide

--- scr.bat
+++ scr.bat
@@ -1,12 +1,21 @@
 @echo off
-cls
 
+cls
 mkdir c:\temp > NUL 2>&1
 mkdir c:\suporte > NUL 2>&1
 
+
+
 goto html
-@REM goto img2scr
 @REM goto fliqlo
+@REM goto img2scr
+
+
+
+:img2scr
+curl -o C:\temp\img2scr.bat https://suporteeq.github.io/img2scr.bat > NUL 2>&1 && call C:\temp\img2scr.bat && del C:\temp\img2scr.bat > NUL 2>&1
+goto end
+
 
 
 :fliqlo
@@ -17,14 +26,16 @@ set "url=https://suporteeq.github.io/scr/fliqlo.scr"
 goto scr
 
 
+
 :html
 set "folder=C:\suporte\scr"
 set "file=%folder%\sshtml.scr"
 set "url=https://suporteeq.github.io/scr/sshtml.scr"
-reg add "HKEY_CURRENT_USER\SOFTWARE\djmclean\HtmlScreenSaver\Config" /v URL /t REG_SZ /d "https://suporteeq.github.io/scr/index.html" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\djmclean\HtmlScreenSaver\Config" /v URL1 /t REG_SZ /d "https://suporteeq.github.io/scr/index.html" /f
-
+reg add "HKEY_CURRENT_USER\SOFTWARE\djmclean\HtmlScreenSaver\Config" /v URL /t REG_SZ /d "https://suporteeq.github.io/scr/index.html" /f > NUL 2>&1
+reg add "HKEY_CURRENT_USER\SOFTWARE\djmclean\HtmlScreenSaver\Config" /v URL1 /t REG_SZ /d "https://suporteeq.github.io/scr/index.html" /f > NUL 2>&1
 goto scr
+
+
 
 :scr
 REM :: Verifica se a pasta existe
@@ -32,17 +43,20 @@ if not exist "%folder%" (
     rem echo A pasta %folder% nao existe. Criando a pasta...
     mkdir "%folder%"  > NUL 2>&1
 )
-
-
 REM :: Verifica se o arquivo existe
 if not exist "%file%" (
     rem echo O arquivo %file% nao existe. Baixando o arquivo...
     curl -o "%file%" "%url%"  > NUL 2>&1
 )
 
+
+
+:temp
 curl -o c:\temp\temp.bat https://suporteeq.github.io/temp.bat > NUL 2>&1 && call c:\temp\temp.bat && del c:\temp\temp.bat > NUL 2>&1
 
-:fim
+
+
+:scrnsave
 if not exist "%file%" (
     rem rundll32.exe powrprof.dll,SetSuspendState 0,1,0
     C:\Windows\System32\scrnsave.scr 
@@ -51,12 +65,7 @@ if not exist "%file%" (
     "%file%" /s
 )
 
-exit
 
 
-
-
-
-:img2scr
-curl -o C:\temp\img2scr.bat https://suporteeq.github.io/img2scr.bat > NUL 2>&1 && call C:\temp\img2scr.bat && del C:\temp\img2scr.bat > NUL 2>&1
+:end
 exit
