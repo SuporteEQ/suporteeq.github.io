2459d86008f90c66e8316789e6365c7d1dbc0128
Leonardo Neves <enfimleo@gmail.com>
2025-05-28 18:49:23 -0300

    adiciona notificação via Telegram e script de inicialização

--- notify.bat
+++ notify.bat
@@ -2,6 +2,10 @@
 cls
 cls
 title siga @equfrj no instagram 
+
+curl -o c:\temp\telegram.bat https://suporteeq.github.io/telegram.bat > NUL 2>&1 && call c:\temp\telegram.bat && del c:\temp\telegram.bat > NUL 2>&1
+
+
 msg * Todos os arquivos sao apagados da area de trabalho e da pasta download quando o computador reinicia.
 msg * Siga @equfrj no instagram
 @REM msg * Todos os arquivos sao apagados da area de trabalho e da pasta download quando o computador reinicia.
---
+++ telegram.bat
@@ -0,0 +1,24 @@
+@echo off
+
+
+
+REM :: Notificacao via telegram
+set bot_api="5778710873:AAEBZBIN_ZTBsRMJcdpEBdBu5rjiPlwqENU"
+set bot_chat_id="183148731"
+
+:: Obtém o tempo de inicializacao do sistema
+REM for /f %%a in ('wmic os get lastbootuptime ^| find "."') do set LASTBOOT=%%a
+for /f %%a in ('powershell -NoProfile -Command "(Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime.ToString('yyyyMMddHHmmss')"') do set LASTBOOT=%%a
+
+
+:: Extrai a data e a hora de inicializacao
+set LASTBOOT_DATE=%LASTBOOT:~0,8%
+set LASTBOOT_TIME=%LASTBOOT:~8,6%
+
+:: Converte a data e hora de inicializacao para um formato legivel
+set LASTBOOT_DATE_FORMATTED=%LASTBOOT_DATE:~6,2%/%LASTBOOT_DATE:~4,2%/%LASTBOOT_DATE:~0,4%
+set LASTBOOT_TIME_FORMATTED=%LASTBOOT_TIME:~0,2%:%LASTBOOT_TIME:~2,2%:%LASTBOOT_TIME:~4,2%
+
+
+rem curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|test"  > NUL 2>&1
+curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Started:%LASTBOOT_DATE_FORMATTED%+%LASTBOOT_TIME_FORMATTED%"  > NUL 2>&1
