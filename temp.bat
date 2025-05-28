a9f56366448b0961cde6f71edc7c120c73a4d275
Leonardo Neves <enfimleo@gmail.com>
2025-05-28 19:37:29 -0300

    atualiza script para incluir intervalo de execução e notificação via Telegram

--- temp.bat
+++ temp.bat
@@ -1,97 +1,51 @@
 @echo off
-
 title siga @equfrj no instagram
-
 setlocal
 
-REM psexec.exe -accepteula -i -u suporte -p SENHA c:\suporte-eq\bin\...bat && cls
+rem Defina aqui o intervalo de datas e horários
+set START_DATE=20250528
+set END_DATE=20250628
+set START_TIME=00:01
+set END_TIME=23:59
 
-wget -O C:\suporte\netboot.xyz.iso https://boot.netboot.xyz/ipxe/netboot.xyz.iso --no-check-certificate
+set bot_api=5778710873:AAEBZBIN_ZTBsRMJcdpEBdBu5rjiPlwqENU
+set bot_chat_id=183148731
 
-
-set TARGET_DATE=20250528
-set START_TIME=00:00
-set END_TIME=23:00
-
-REM :: Notificacao via telegram
-set bot_api="5778710873:AAEBZBIN_ZTBsRMJcdpEBdBu5rjiPlwqENU"
-set bot_chat_id="183148731"
-
-REM for /f %%a in ('wmic os get localdatetime ^| find "."') do set CURRENT_DATETIME=%%a
 for /f %%a in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMddHHmmss"') do set CURRENT_DATETIME=%%a
-
-
 set CURRENT_DATE=%CURRENT_DATETIME:~0,8%
-set CURRENT_HOUR=%CURRENT_DATETIME:~8,2%
-set CURRENT_MIN=%CURRENT_DATETIME:~10,2%
-set CURRENT_TIME=%CURRENT_HOUR%:%CURRENT_MIN%
+set CURRENT_TIME=%CURRENT_DATETIME:~8,2%:%CURRENT_DATETIME:~10,2%
 
-rem echo Dia atual: %CURRENT_DATE%
-rem echo Hora atual: %CURRENT_HOUR%:%CURRENT_MIN%
-
-
-
-:: Obtém o tempo de inicializacao do sistema
-REM for /f %%a in ('wmic os get lastbootuptime ^| find "."') do set LASTBOOT=%%a
 for /f %%a in ('powershell -NoProfile -Command "(Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime.ToString('yyyyMMddHHmmss')"') do set LASTBOOT=%%a
-
-
-:: Extrai a data e a hora de inicializacao
 set LASTBOOT_DATE=%LASTBOOT:~0,8%
-set LASTBOOT_TIME=%LASTBOOT:~8,6%
+set LASTBOOT_TIME=%LASTBOOT:~8,2%:%LASTBOOT:~10,2%:%LASTBOOT:~12,2%
 
-:: Converte a data e hora de inicializacao para um formato legivel
-set LASTBOOT_DATE_FORMATTED=%LASTBOOT_DATE:~6,2%/%LASTBOOT_DATE:~4,2%/%LASTBOOT_DATE:~0,4%
-set LASTBOOT_TIME_FORMATTED=%LASTBOOT_TIME:~0,2%:%LASTBOOT_TIME:~2,2%:%LASTBOOT_TIME:~4,2%
+set EXECUTE=NOT
 
-:: Exibe o tempo de atividade desde a última inicializacao
-REM echo O sistema foi inicializado em: %LASTBOOT_DATE_FORMATTED% %LASTBOOT_TIME_FORMATTED%
-
-:: Usa PowerShell para calcular o uptime
-for /f %%u in ('powershell -command "(get-date) - (gcim Win32_OperatingSystem).LastBootUpTime"') do set UPTIME=%%u
-
-
-
-
-if "%CURRENT_DATE%"=="%TARGET_DATE%" (
+rem Verifica se a data atual está entre a data inicial e final (inclusive)
+if "%CURRENT_DATE%" GEQ "%START_DATE%" if "%CURRENT_DATE%" LEQ "%END_DATE%" (
+    rem Verifica se a hora atual está entre hora inicial e final (inclusive)
     if "%CURRENT_TIME%" GEQ "%START_TIME%" if "%CURRENT_TIME%" LEQ "%END_TIME%" (
-        goto exec_comando
-    ) else (
-        goto fora_do_horario
+        set EXECUTE=YES
     )
-) else (
-    goto data_errada
 )
 
+if "%EXECUTE%"=="YES" (
+    echo Executando comando...
+    start /b "" curl.exe -L -o "C:\Suporte\iso\disk-udpcd-receiver.iso" "https://suporteeq.github.io/util/disk-udpcast-precfg.iso" >nul 2>&1
+    start /b "" curl.exe -L -o "C:\suporte\netboot.xyz.iso" "https://boot.netboot.xyz/ipxe/netboot.xyz.iso"  >nul 2>&1
+    start /b "" curl.exe -L -o "C:\suporte\iso\netboot.xyz.iso" "https://boot.netboot.xyz/ipxe/netboot.xyz.iso"  >nul 2>&1
+    curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Started:%LASTBOOT_DATE%+%LASTBOOT_TIME%|Exec:done" >NUL 2>&1
+) else (
+    echo Fora do intervalo de data ou horario
+    curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Started:%LASTBOOT_DATE%+%LASTBOOT_TIME%|Exec:not_executed" >NUL 2>&1
+)
 
-:exec_comando
-:: Coloque aqui o comando que você deseja executar
-rem echo Hoje SIM
-rem shutdown /s /t 120
-curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Started:%LASTBOOT_DATE_FORMATTED%+%LASTBOOT_TIME_FORMATTED%|Exec:temp.bat"  > NUL 2>&1
-rem call \\10.30.155.1\share\openlca.bat
-rem curl -o C:\suporte\iso\netboot.xyz.iso https://boot.netboot.xyz/ipxe/netboot.xyz.iso --silent --fail --retry 3
-rem curl -L -o "C:\Suporte\iso\disk-udpcd-receiver.iso" "https://github.com/SuporteEQ/suporteeq.github.io/raw/refs/heads/master/util/disk-udpcast-precfg.iso"
-curl.exe -L -o "C:\Suporte\iso\disk-udpcd-receiver.iso" "https://suporteeq.github.io/util/disk-udpcast-precfg.iso"
+rem ALWAYS
+@REM start /b "" curl.exe -L -o "C:\Suporte\iso\disk-udpcd-receiver.iso" "https://suporteeq.github.io/util/disk-udpcast-precfg.iso" >nul 2>&1
+@REM start /b "" curl.exe -L -o "C:\suporte\netboot.xyz.iso" "https://boot.netboot.xyz/ipxe/netboot.xyz.iso"  >nul 2>&1
+@REM start /b "" curl.exe -L -o "C:\suporte\iso\netboot.xyz.iso" "https://boot.netboot.xyz/ipxe/netboot.xyz.iso"  >nul 2>&1
 
-goto fim
-
-:fora_do_horario
-rem echo Agora NAO
-rem curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Started:%LASTBOOT_DATE_FORMATTED%+%LASTBOOT_TIME_FORMATTED%|Exec:HoraErrada"  > NUL 2>&1
-
-goto fim
-
-:data_errada
-rem echo Hoje NAO
-rem curl -s "https://api.telegram.org/bot%bot_api%/sendMessage?chat_id=%bot_chat_id%&disable_notification=true&text=Computer:%computername%|User:%username%|Started:%LASTBOOT_DATE_FORMATTED%+%LASTBOOT_TIME_FORMATTED%|Exec:DiaErrado"  > NUL 2>&1
-goto fim
-
-:fim
-cls
-rem echo --------------------------
-rem echo siga @equfrj no instagram
-rem echo --------------------------
-rem timeout /t 60
 
 echo %date% %time% >> c:\suporte\%~n0.txt
+pause
+cls
