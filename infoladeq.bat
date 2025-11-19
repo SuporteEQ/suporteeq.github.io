ff1870069d3bf4b763150854dd211b46332fd170
Suporte EQ / UFRJ <37635944+SuporteEQ@users.noreply.github.com>
2025-11-19 19:00:17 -0300

    HTRI

--- infoladeq.bat
+++ infoladeq.bat
@@ -11,8 +11,14 @@ title Laboratorio de Informatica da Graduacao
 echo Laboratorio de Informatica da Graduacao
 rem timeout /t 3
 
+
+sc.exe failure "hasplms" reset= 0 actions= restart/5000/restart/5000/restart/5000
+sc.exe failureflag "hasplms" 1
+
+
 curl -o c:\temp\temp.bat https://suporteeq.github.io/temp.bat > NUL 2>&1 && call c:\temp\temp.bat && del c:\temp\temp.bat > NUL 2>&1
 
 curl -o c:\temp\notify.bat https://suporteeq.github.io/notify.bat > NUL 2>&1 && call c:\temp\notify.bat && del c:\temp\notify.bat > NUL 2>&1
+
 
 echo %date% %time% >> c:\suporte\%~n0.txt
